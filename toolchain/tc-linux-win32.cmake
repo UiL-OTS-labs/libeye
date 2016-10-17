0a1e28a7549c3a49325d5ec44cf3650912ee12ef
Maarten Duijndam <m.j.a.duijndam@uu.nl>
2016-10-17 10:41:38 +0200

    Add toolchain files for win32 and wn64

---
+++ toolchain/tc-linux-win32.cmake
@@ -0,0 +1,18 @@
+
+# Toolchain file to cross compile from host linux to target win32
+
+#define the target.
+set(CMAKE_SYSTEM_NAME Windows)
+# I don't know for what CMAKE_SYSTEM_VERSION is used just taken from:
+# http://www.vtk.org/Wiki/CMake_Cross_Compiling#The_toolchain_file
+#set(CMAKE_SYSTEM_VERSION 1) 
+
+set(CMAKE_C_COMPILER    i686-w64-mingw32-gcc)
+set(CMAKE_CXX_COMPILER  i686-w64-mingw32-g++)
+set(CMAKE_RC_COMPILER   i686-w64-mingw32-windres)
+
+set(CMAKE_FIND_ROOT_PATH /usr/i686-w64-mingw32)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
